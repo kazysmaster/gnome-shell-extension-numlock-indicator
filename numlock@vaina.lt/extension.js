74c8bf325d36b121fd8ec8789b4283d5b8aa0f70
Kazimieras Vaina <kazimieras.vaina@gmail.com>
2011-11-21 02:02:49 +0200

    Notifications implemented

--- numlock@vaina.lt/extension.js
+++ numlock@vaina.lt/extension.js
@@ -1,15 +1,15 @@
 const St = imports.gi.St;
 const Lang = imports.lang;
-const PanelMenu = imports.ui.panelMenu;
-const PopupMenu = imports.ui.popupMenu;
-const Panel = imports.ui.panel;
-const Main = imports.ui.main;
-const Keymap = imports.gi.Gdk.Keymap;
-
-//imports.gi.Gdk.Keymap.get_default().get_num_lock_state();
-
 const Gettext = imports.gettext;
 const _ = Gettext.gettext;
+
+const Keymap = imports.gi.Gdk.Keymap;
+
+const Panel = imports.ui.panel;
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+const MessageTray = imports.ui.messageTray;
 
 let indicator;
 
@@ -46,17 +46,57 @@ NumlockIndicator.prototype = {
 		this.menu.addMenuItem(this.menuItem);
 
 		this._updateState();
-		Keymap.get_default().connect('state-changed', Lang.bind(this, this._updateState));
+		Keymap.get_default().connect('state-changed', Lang.bind(this, this._handleStateChange));
 	},
+	
+	_handleStateChange: function(actor, event) {
+		if (this.numlock_state != this._getNumlockState()) {
+			this._showNotification();
+		}
+		this._updateState();
+	}, 
 
-	_updateState: function(actor, event) {
-		//this.label.set_text( this._getNumlockStateText() );
+	_updateState: function() {
+		this.numlock_state = this._getNumlockState();
 		this.label.set_style_class_name( this._getNumlockStateClassName() );
 		this.menuItem.setToggleState( this._getNumlockState() );
 	},
-
+	
 	_getNumlockStateClassName: function() {
 		return this._getNumlockState() ? 'numlock-state-enabled' : 'numlock-state-disabled';
+	},
+	
+	_showNotification: function() {
+		this._prepareSource();
+		
+		let notification_text = _('Numlock') + ' ' + this._getNumlockStateText();
+		let notification = null;
+		if (this._source.notifications.length == 0) {
+			notification = new MessageTray.Notification(this._source, notification_text);
+			notification.setTransient(true);
+			notification.setResident(false);
+		} else {
+			notification = this._source.notifications[0];
+            notification.update(notification_text, null, { clear: true });
+		}
+		
+		this._source.notify(notification);
+	},
+	
+	_prepareSource: function() {
+		if (this._source == null) {
+			this._source = new MessageTray.SystemNotificationSource();
+			this._source.createNotificationIcon = function() {
+				return new St.Icon({ icon_name: 'input-keyboard',
+									 icon_type: St.IconType.SYMBOLIC,
+									 icon_size: this.ICON_SIZE });
+			};
+			this._source.connect('destroy', Lang.bind(this,
+				function() {
+					this._source = null;
+				}));
+			Main.messageTray.add(this._source);
+		}
 	},
 
 	 _getNumlockStateText: function() {
